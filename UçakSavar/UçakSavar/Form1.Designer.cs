c6825a8355e7f699613edaf97c5f446dd69d17f1
beytullah-gn <b-gnydn_@hotmail.com>
2022-01-31 13:23:57 +0300

    Main

---
+++ UçakSavar/UçakSavar/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace UçakSavar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.skorlabel = new System.Windows.Forms.Label();
+            this.Sayı = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // skorlabel
+            // 
+            this.skorlabel.AutoSize = true;
+            this.skorlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.skorlabel.Location = new System.Drawing.Point(21, 20);
+            this.skorlabel.Name = "skorlabel";
+            this.skorlabel.Size = new System.Drawing.Size(74, 25);
+            this.skorlabel.TabIndex = 0;
+            this.skorlabel.Text = "Skor =";
+            // 
+            // Sayı
+            // 
+            this.Sayı.AutoSize = true;
+            this.Sayı.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Sayı.Location = new System.Drawing.Point(101, 20);
+            this.Sayı.Name = "Sayı";
+            this.Sayı.Size = new System.Drawing.Size(19, 25);
+            this.Sayı.TabIndex = 1;
+            this.Sayı.Text = "-";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 681);
+            this.Controls.Add(this.Sayı);
+            this.Controls.Add(this.skorlabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label skorlabel;
+        private System.Windows.Forms.Label Sayı;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ UçakSavar/UçakSavar/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UçakSavar.Properties;
+
+namespace UçakSavar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        PictureBox ucak = new PictureBox();
+        PictureBox ucaksavar = new PictureBox();
+        
+        ArrayList mermilistesi = new ArrayList();
+        ArrayList düşmanlistesi = new ArrayList();
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ucaksavar.Image = Resources.ucaksavar;
+            this.Controls.Add(ucaksavar);
+            ucaksavar.Location = new Point(550, 630);
+            timer1.Enabled = true;
+            timer3.Enabled = true;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UçakOluştur();
+            foreach (PictureBox item in düşmanlistesi)
+            {
+                this.Controls.Add(item);
+            }
+            uçakhareket();
+            
+        }
+
+        private void uçakhareket()
+        {
+            foreach (PictureBox item in düşmanlistesi)
+            {
+                Point konum = item.Location;
+                konum.Y += 15;
+                item.Location = konum;
+
+
+            }
+        }
+
+        private void UçakOluştur()
+        {
+            Random rnd = new Random();
+            PictureBox uçak = new PictureBox();
+            uçak.Image = Resources.ucak;
+            int konum = rnd.Next(0, 1200);
+            int konum2 = rnd.Next(0, 15);
+            uçak.Location = new Point(konum, konum2);
+            düşmanlistesi.Add(uçak);
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Point konum = ucaksavar.Location;
+            if(e.KeyCode  ==Keys.A && (ucaksavar.Location.X>0 && ucaksavar.Location.X <1200) )
+            {
+                konum.X -= 8;
+                ucaksavar.Location = konum;
+            }
+            if (e.KeyCode == Keys.D && (ucaksavar.Location.X > 0 && ucaksavar.Location.X < 1200))
+            {
+                konum.X += 8;
+                ucaksavar.Location = konum;
+            }
+            if (e.KeyCode == Keys.Space )
+            {
+                BombaÜret();
+                timer2.Enabled = true;
+                
+
+            }
+        }
+
+        private void BombaÜret()
+        {
+            PictureBox mermi = new PictureBox();
+            mermi.Image = Resources.mermi1;
+            mermi.Location = ucaksavar.Location;
+            mermilistesi.Add(mermi);
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            
+            foreach (PictureBox item in mermilistesi)
+            {
+                this.Controls.Add(item);
+            }
+
+            BombaHareket();
+            
+
+        }
+
+        private void BombaHareket()
+        {
+            foreach (PictureBox item in mermilistesi)
+            {
+                Point konum = item.Location;
+                konum.Y -= 5;
+                item.Location = konum;
+            }
+        }
+
+        int skor = 0;
+        PictureBox kaldirilanucaklar = new PictureBox();
+        PictureBox kaldirilanmermiler = new PictureBox();
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            foreach (PictureBox item1 in mermilistesi)
+            {
+                foreach (PictureBox item2 in düşmanlistesi)
+                {
+                    if(item1.Bounds.IntersectsWith(item2.Bounds))
+                    {
+                        this.Controls.Remove(item1);
+                        this.Controls.Remove(item2);
+                        kaldirilanmermiler = item1;
+                        kaldirilanucaklar = item2;
+                        skor += 1;
+                    }
+                }
+            }
+            mermilistesi.Remove(kaldirilanmermiler);
+            düşmanlistesi.Remove(kaldirilanucaklar);
+            Sayı.Text = skor.ToString();
+        }
+    }
+}
